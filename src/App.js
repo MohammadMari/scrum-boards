2b05d86886da11beff5c01abc145ca6a35fe58fd
Saul G <saulgaray@my.unt.edu>
2022-06-29 13:42:38 -0500

    Firestore DB basic setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+
+import { accountConverter } from './Account';
+
+//User account class
+
+//Firebase config
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDONLZ1Wzj7zT-GHqmzhGZt2_Re-jMbY68",
+  authDomain: "scrum-board-b3df8.firebaseapp.com",
+  projectId: "scrum-board-b3df8",
+  storageBucket: "scrum-board-b3df8.appspot.com",
+  messagingSenderId: "422339418451",
+  appId: "1:422339418451:web:bd1098b52732f2858b29a6",
+  measurementId: "G-B5LXW3264P"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 function App() {
+
+  dbTest();
+
   return (
-     <div className='navbar'>
+    <div className='navbar'>
       <button className='navButton'>
         Dashboard
       </button>
       <button className='navButton'>
         Board
       </button>
-         
-      </div>
+
+    </div>
   );
+}
+
+async function dbTest() {
+  const querySnapshot = await getDocs(collection(db, "auth").withConverter(accountConverter));
+  querySnapshot.forEach((doc) => {
+    const acc = doc.data();
+    console.log(acc.toString());
+  });
 }
 
 export default App;
